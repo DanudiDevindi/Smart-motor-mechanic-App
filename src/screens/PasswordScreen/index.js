7ef98c7de47a1b50825b9c6d02983ced43372ea6
Danudi <danusilva1997@gmail.com>
2022-01-05 12:06:01 +0530

    password screen

---
+++ src/screens/PasswordScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+const PasswordScreen =()=>{
+    return(
+        <View>
+            <Text>PasswordScreen</Text>
+        </View>
+    )
+}
+
+export default PasswordScreen;
