32f6fcdc4f9fb12b218ba106a1da72c6b51e982c
Danudi <danusilva1997@gmail.com>
2022-01-05 09:15:25 +0530

    sign in screen

--- src/screens/SignINScreen/index.js
+++ src/screens/SignINScreen/index.js
@@ -86,21 +86,7 @@ const SignINScreen = ({ navigation }) => {
         }
     }
 
-    // const paypalIntegrate = () => {
-    //     axios.post('https://api.sandbox.paypal.com/v1/oauth2/token', { grant_type: 'client_credentials' },
-    //         {
-    //             headers: {
-    //                 'Content-Type': 'application/x-www-form-urlencoded',
-    //                 'Authorization': ` ` // your Authorization value you get from postman api hit
-    //             }
-    //         }
-    //     )
-    //         .then(response => {
-    //             console.log(response.data.access_token)
-    //         }).catch(err => {
-    //             console.log({ ...err })
-    //         })
-    // }
+    
 
     if(data.isLoading){
         return (
