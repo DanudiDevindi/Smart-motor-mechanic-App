2c325cc1b80b67f1261341217bb88858de2e07db
Danudi <danusilva1997@gmail.com>
2022-01-11 10:57:28 +0530

    add service

--- src/screens/AddServiceScreen/index.js
+++ src/screens/AddServiceScreen/index.js
@@ -97,8 +97,105 @@ const AddServicescreen = ({ route, navigation }) => {
             </View>
         )
     }
+    const ServiceTypeItemView = ({ item, index }) => {
+        return (
+            <View style={{ flex: 0.5 }}>
+                <CheckBoxx
+                    txt={item.type}
+                    value={data.service_type_id === item.service_type_id}
+                    onValueChange={() =>
+                        setData({
+                            ...data,
+                            service_type: item.type,
+                            service_type_id: item.service_type_id,
+                            price_set_as: item.price_set_as,
+                            service_type_error: ''
+                        })
+                    }
+                />
+            </View>
+        )
+    }
 
-    
+    const saveService = () => {
+        // if (data.cat_id === 0 || data.title === '' || data.description === '' || data.service_type === '' || data.isSelectImage===false) {
+        if (data.cat_id === 0 || data.title === '' || data.description === '' || data.service_type === '' || region.region === '' || data.isSelectImage === false) {
+            setData({
+                ...data,
+                cat_error: data.cat_id === 0 ? "Please select category" : "",
+                title_error: data.title === "" ? "Please enter title" : "",
+                description_error: data.description === "" ? "Please add description" : "",
+                service_type_error: data.service_type === "" ? "Please select service type" : "",
+                location_err: region.region === '' ? "Please select Location" : '',
+                image_error: data.isSelectImage === false ? "Please select image" : "",
+            });
+
+        } else {
+            setData({
+                ...data,
+                showIndicator: true
+            })
+            const values = new FormData();
+            if (image1.name!==""){
+                values.append('files', { uri: image1.uri, name: image1.name, type: image1.type });
+            }
+            if (image2.name!==""){
+                values.append('files', { uri: image2.uri, name: image2.name, type: image2.type });
+            }
+            if (image3.name!==""){
+                values.append('files', { uri: image3.uri, name: image3.name, type: image3.type });
+            }
+                
+            values.append('data', JSON.stringify(data));
+            values.append('location', JSON.stringify(region))
+            axios.request({
+                url: url + 'createService',
+                method: 'POST',
+                headers: {
+                    Accept: "application/json",
+                    'Content-Type': 'application/octet-stream'
+                },
+                data: values,
+
+            }).then((response) => {
+                if (response.data.err === false) {
+                    if (response.data.status === "payment_done") {
+                        isVisible(true)
+                    } else if (response.data.status === "payment_not_done") {
+                        navigation.navigate("Payment", { service_typeID: data.service_type_id })
+                    }
+                    setData({
+                        ...data,
+                        server_err: response.data.message,
+                        isSucess: true,
+                        location_err: '',
+                        showIndicator: false,
+                        cat_id: 0,
+                        cat_name: '',
+                        title: '',
+                        description: '',
+                        service_type_id: 0,
+                        price: 0,
+                    });
+                } else {
+                    setData({
+                        ...data,
+                        server_err: response.data.message,
+                        isSucess: false,
+                        showIndicator: false
+                    });
+                }
+            }).catch((error) => {
+                setData({
+                    ...data,
+                    server_err: "Server Error",
+                    isSucess: false,
+                    showIndicator: false
+                });
+            })
+        }
+    }
+
 
 }
 
