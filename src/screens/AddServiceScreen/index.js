74a47fad7f67977dbf51351b41b354cc8265ce61
Danudi <danusilva1997@gmail.com>
2022-01-11 10:45:40 +0530

    add service screen

---
+++ src/screens/AddServiceScreen/index.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, TextInput, Image, FlatList, TouchableOpacity, ImageBackground, Alert, ScrollView, Modal, ActivityIndicator } from 'react-native';
+import { styles } from './styles';
+import CheckBoxx from '../../component/CheckBoxx';
+import BigButton from '../../component/BigButton';
+import url from '../../api/url';
+import imag_url from '../../api/imageURL';
+import axios from 'axios';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import ErrorMsg from '../../component/ErrorMsg';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Feather from 'react-native-vector-icons/Feather';
+
+const AddServicescreen = ({ route, navigation }) => {
+
+    var { region } = '';
+    { route === undefined ? null : region = route.params }
+    const [vis, isVisible] = useState(false)
+    const [cat, setCat] = useState([]);
+    const [service_types, setServiceType] = useState([]);
+    const [image1, setImage1] = useState({
+        uri: imag_url + "/upload_image.png",
+        type: "",
+        name: "",
+    });
+    const [image2, setImage2] = useState({
+        uri: imag_url + "/upload_image.png",
+        type: "",
+        name: "",
+    });
+    const [image3, setImage3] = useState({
+        uri: imag_url + "/upload_image.png",
+        type: "",
+        name: "",
+    });
+    const [data, setData] = useState({
+        cat_id: 0,
+        cat_name: '',
+        cat_error: '',
+        title: '',
+        title_error: '',
+        description: '',
+        description_error: '',
+        service_type_id: 0,
+        service_type: '',
+        price_set_as: '',
+        service_type_error: '',
+        price: 0,
+        price_error: '',
+        status: '',
+        isSelectImage: false,
+        image_error: '',
+        server_err: '',
+        isSucess: false,
+        location_err: '',
+        showIndicator: false,
+        createAt: new Date()
+    })
+
+    
+
+
+}
+
+export default AddServicescreen;
