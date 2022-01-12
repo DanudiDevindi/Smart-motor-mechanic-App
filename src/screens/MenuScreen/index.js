b12ab1a466ce9bda11eb4fbb937765b796cbd82b
Danudi <danusilva1997@gmail.com>
2022-01-12 16:02:10 +0530

    menu screen

---
+++ src/screens/MenuScreen/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text,View } from 'react-native';
+
+const MenuScreen=()=>{
+    return(
+        <Text>Sample MenuScreen</Text>
+    )
+}
+
+export default MenuScreen;
