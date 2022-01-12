aae4e4c79a6875f9e181cda1f4ab113489aeb76a
Danudi <danusilva1997@gmail.com>
2022-01-12 10:48:45 +0530

    get user types

---
+++ src/screens/EditServiceScreen/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, TextInput, Image, FlatList, TouchableOpacity, ImageBackground, Alert, ScrollView ,ActivityIndicator} from 'react-native';
+import { styles } from './styles';
+import CheckBoxx from '../../component/CheckBoxx';
+import BigButton from '../../component/BigButton';
+import url from '../../api/url';
+import imag_url from '../../api/imageURL';
+import axios from 'axios';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import ErrorMsg from '../../component/ErrorMsg';
+
+const EditServiceScreen = ({ route, navigation }) => {
+
+    var { region } = route.params;
+    const { item } = route.params;
+    console.log(item)
+    const [cat, setCat] = useState([]);
+    const [service_types, setServiceType] = useState([]);
+    const [images, setImages] = useState({
+        uri: imag_url + item.image,
+        type: "",
+        name: "",
+        isSelectImage:false,
+        image_error: ''
+    });
+    const [data, setData] = useState({
+        service_id: item.service_id,
+        cat_id: item.cat_id,
+        cat_name: item.category,
+        cat_error: '',
+        title: item.title,
+        title_error: '',
+        description: item.description,
+        description_error: '',
+        service_type: item.service_type,
+        service_type_error: '',
+        price: item.price,
+        price_error: '',
+        status: item.status,
+        image: imag_url + item.image,
+        image_error: '',
+        longitude: item.longitude,
+        latitude: item.latitude,
+        server_err: '',
+        isSucess: false,
+        showIndicator: false,
+        isLocationSelect:false,
+        isImageSelect:false
+    })
+    const getServiceTypes = () => {
+        fetch(url + 'AllService_types').then((response) => response.json()).then((responseJson) => {
+            setServiceType(responseJson);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+    useEffect(() => {
+        getCategories();
+        getServiceTypes();
+    }, []);
+
+
+}
+
+export default EditServiceScreen;
+
