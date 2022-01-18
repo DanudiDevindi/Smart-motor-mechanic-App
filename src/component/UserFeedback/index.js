6375f5f1272f61209285f5392d2b73a821b48fdf
Danudi <danusilva1997@gmail.com>
2022-01-18 16:07:13 +0530

    feed back

---
+++ src/component/UserFeedback/index.js
@@ -0,0 +1,29 @@
+import React,{useState} from 'react';
+import { View, Text } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const UserFeedback = ({ item }) => {
+    console.log(item.rate)
+    const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
+    return (
+        <View style={{ backgroundColor: '#f2f2f2', borderRadius: 10 }}>
+            <View style={{ margin: 15 }}>
+                <Text style={{color:'black'}}>{item.username}</Text>
+                <View style={{ flexDirection: 'row',marginVertical:5 }}>
+                {
+                    maxRating.map((item1, key) => {
+                        return (
+                            <MaterialIcons name={item1 <= item.rate ? "star" : "star-border"} size={20} color="#FF8546" key={item1} />
+                        )
+
+                    })
+                }
+
+            </View>                
+                <Text style={{fontSize:18}}>{item.comment}</Text>
+            </View>
+        </View>
+    )
+}
+
+export default UserFeedback;
