9083bb3a8b4cb99ad2a33de4f1935b2a1028086d
Danudi <danusilva1997@gmail.com>
2022-01-19 12:30:11 +0530

    data context

---
+++ src/context/createDataContext.js
@@ -0,0 +1,22 @@
+import React,{useReducer} from 'react';
+
+export default (reducer,actions,initialState)=>{
+    const Context=React.createContext();
+
+    const Provider=({children})=>{
+        const [state,dispatch]=useReducer(reducer,initialState);
+
+        //actions==={addBlogPost:(dispatch)=>{return()=>{}}}
+
+        const boundActions={};
+        for(let key in actions){
+            boundActions[key]=actions[key](dispatch);
+        }
+
+        return <Context value={{state}}>
+            {children}
+        </Context>
+    }
+
+    return{Context,Provider};
+};
